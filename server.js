9c85276a35bf794a280bb8b12292cbbcc5b92fad
thomas-tong7 <thomas.chanon7@gmail.com>
2026-03-19 21:21:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,18 +6,19 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-// 🟢 PASTE YOUR GOOGLE DEPLOYMENT URL HERE
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzEhCUXEC-xXFbVX4FbPlCD15KwpwJCtyK19psbEguGiAyxtPokUXSzczr7PoZHrK6n/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGJSoG27RjdQwKBnS46NhNpwWXRHTjw3GSm7aV45WyD0BshZoCvaBVlwRUFJ7-SDJz/exec'; 
 
+// Route for both Save and Delete
 app.post('/api/reserve', async (req, res) => {
     try {
         const response = await axios.post(SCRIPT_URL, req.body);
         res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: "Connect Error" });
+        res.status(500).json({ error: "Google Script Error" });
     }
 });
 
+// Route to fetch all data
 app.get('/api/bookings', async (req, res) => {
     try {
         const response = await axios.get(SCRIPT_URL);
@@ -28,4 +29,4 @@ app.get('/api/bookings', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Muse active on ${PORT}`));
+app.listen(PORT, () => console.log(`Muse Server running on port ${PORT}`));
